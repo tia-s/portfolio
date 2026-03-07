9c86d85ece717ad3e1824af3ed65604a85526a68
tia-s <tiakristensalmon@gmail.com>
2026-03-07 18:13:38 -0400

    Added footer section

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,7 @@
     <ThemeToggle />
     <HeroSection />
     <ProjectsSection />
+    <FooterSection />
   </div>
 </template>
 
@@ -11,6 +12,7 @@ import { onMounted } from 'vue'
 import HeroSection from './components/HeroSection.vue'
 import ProjectsSection from './components/ProjectsSection.vue'
 import ThemeToggle from './components/ThemeToggle.vue'
+import FooterSection from './components/FooterSection.vue'
 import { useTheme } from './composables/useTheme'
 
 const { init } = useTheme()
---
+++ client/src/components/FooterSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <p class="footer__text">Built with Vue.js + TypeScript and FastAPI</p>
+    <p class="footer__text">Deployed with Netlify and Railway</p>
+  </footer>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/components/footer';
+</style>
